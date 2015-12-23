c5a8a3aad66a8834c8e3f5ab93c4b5b28c0eb5ef
zeeshan <ziishaned@gmail.com>
2015-12-23 15:58:59 +0500

    UI modification

--- application/views/rsql.php
+++ application/views/rsql.php
@@ -27,54 +27,73 @@
 				        	<div class="panel panel-default dbconPanel">
 				        		<div class="container">
 					        		<div class="panel-body">
-					        		   <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-					        		   		<div class="page-header condbHead">
-					        		   			<h1>Connect to <small>DB</small></h1>
-					        		   		</div>
-					        		   		<div class="page-body condbBody">
-					        		   			<div class="row">
-					        		   				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-					        		   					<?php $attribute = array('role' => 'form'); echo form_open('rsql/db_setting', $attribute); ?>
-							        		   				<div class="row">
-							        		   					<div class="col-lg-6 col-lg-offset-0 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-0 col-lg-6 col-lg-offset-0">
-									        		   				<div class="form-group">
-									        		   					<label for="hostname"><span class="glyphicon glyphicon-hdd" aria-hidden="true"></span> HOST/IP Address: </label>
-									        		   					<?php $attribute = array('name' => 'hostname', 'class' => 'form-control', 'id' => 'hostname', 'required' => 'required', 'value' => '127.0.0.1'); echo form_input($attribute); ?>
-									        		   				</div>
-							        		   					</div>
-								        		   				<div class="col-lg-6 col-lg-offset-0 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-0 col-lg-6 col-lg-offset-0">
-									        		   				<div class="form-group">
-									        		   					<label for="port"><span class="glyphicon glyphicon-inbox" aria-hidden="true"></span> DB Port: </label>
-									        		   					<?php $attribute = array('name' => 'port', 'class' => 'form-control', 'id' => 'port', 'required' => 'required', 'value' => '3306'); echo form_input($attribute); ?>	
-									        		   				</div>
-							        		   					</div>
-							        		   				</div>
-							        		   				<div class="row">
-							        		   					<div class="col-lg-6 col-lg-offset-0 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-0 col-lg-6 col-lg-offset-0">
-									        		   				<div class="form-group">
-									        		   					<label for="dbname"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> DB Name: </label>
-									        		   					<?php $attribute = array('name' =>'dbname', 'class' => 'form-control', 'id' => 'dbName', 'required' => 'required', 'value' => ''); echo form_input($attribute); ?>
-									        		   				</div>
-							        		   					</div>
-							        		   					<div class="col-lg-6 col-lg-offset-0 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-0 col-lg-6 col-lg-offset-0">
-									        		   				<div class="form-group">
-									        		   					<label for="dbUsername"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> DB Username: </label>
-									        		   					<?php $attribute = array('name' =>'dbusername', 'class' => 'form-control', 'id' => 'dbUsername', 'required' => 'required', 'value' => 'root'); echo form_input($attribute); ?>
-									        		   				</div>
-							        		   					</div>
-								        		   				<div class="col-lg-6 col-lg-offset-0 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-0 col-lg-6 col-lg-offset-0">
-									        		   				<div class="form-group">
-									        		   					<label for="dbpassword"><span class="glyphicon glyphicon-lock" aria-hidden="true"></span> DB Password: </label>
-									        		   					<?php $attribute = array('name' =>'dbpassword', 'class' => 'form-control', 'id' => 'dbpassword'); echo form_input($attribute); ?>
-									        		   				</div>
-							        		   					</div>
-							        		   				</div>
-							        		   				<?php $attribute = array('class' => 'btn btn-primary pull-right', 'value' => 'Save Settings'); echo form_submit($attribute); ?>
-							        		   			<?php echo form_close(); ?>
-					        		   				</div>
-					        		   			</div>
-					        		   		</div>
-					        		   </div>
+					        			<?php if ($this->session->userdata('current_db') AND $this->session->userdata('db_slug')) { ?>
+											<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+						        		   		<div class="page-header condbHead">
+						        		   			<h1>Uhhh, So You want to Connect <small>DB!</small></h1>
+												</div>	
+												<div class="page-body condbBody">
+						        		   			<div class="row">
+						        		   				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-8">
+															<ul class="list-group">
+																<li class="list-group-item">Item 1</li>
+																<li class="list-group-item">Item 2</li>
+																<li class="list-group-item">Item 3</li>
+															</ul>
+						        		   				</div>
+						        		   			</div>
+						        		   		</div>
+						        		   	</div>
+					        			<?php } else { ?>
+						        		   	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+						        		   		<div class="page-header condbHead">
+						        		   			<h1>Connect to <small>DB</small></h1>
+						        		   		</div>
+						        		   		<div class="page-body condbBody">
+						        		   			<div class="row">
+						        		   				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+						        		   					<?php $attribute = array('role' => 'form'); echo form_open('rsql/db_setting', $attribute); ?>
+								        		   				<div class="row">
+								        		   					<div class="col-lg-6 col-lg-offset-0 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-0 col-lg-6 col-lg-offset-0">
+										        		   				<div class="form-group">
+										        		   					<label for="hostname"><span class="glyphicon glyphicon-hdd" aria-hidden="true"></span> HOST/IP Address: </label>
+										        		   					<?php $attribute = array('name' => 'hostname', 'class' => 'form-control', 'id' => 'hostname', 'required' => 'required', 'value' => '127.0.0.1'); echo form_input($attribute); ?>
+										        		   				</div>
+								        		   					</div>
+									        		   				<div class="col-lg-6 col-lg-offset-0 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-0 col-lg-6 col-lg-offset-0">
+										        		   				<div class="form-group">
+										        		   					<label for="port"><span class="glyphicon glyphicon-inbox" aria-hidden="true"></span> DB Port: </label>
+										        		   					<?php $attribute = array('name' => 'port', 'class' => 'form-control', 'id' => 'port', 'required' => 'required', 'value' => '3306'); echo form_input($attribute); ?>	
+										        		   				</div>
+								        		   					</div>
+								        		   				</div>
+								        		   				<div class="row">
+								        		   					<div class="col-lg-6 col-lg-offset-0 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-0 col-lg-6 col-lg-offset-0">
+										        		   				<div class="form-group">
+										        		   					<label for="dbname"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> DB Name: </label>
+										        		   					<?php $attribute = array('name' =>'dbname', 'class' => 'form-control', 'id' => 'dbName', 'required' => 'required', 'value' => ''); echo form_input($attribute); ?>
+										        		   				</div>
+								        		   					</div>
+								        		   					<div class="col-lg-6 col-lg-offset-0 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-0 col-lg-6 col-lg-offset-0">
+										        		   				<div class="form-group">
+										        		   					<label for="dbUsername"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> DB Username: </label>
+										        		   					<?php $attribute = array('name' =>'dbusername', 'class' => 'form-control', 'id' => 'dbUsername', 'required' => 'required', 'value' => 'root'); echo form_input($attribute); ?>
+										        		   				</div>
+								        		   					</div>
+									        		   				<div class="col-lg-6 col-lg-offset-0 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-0 col-lg-6 col-lg-offset-0">
+										        		   				<div class="form-group">
+										        		   					<label for="dbpassword"><span class="glyphicon glyphicon-lock" aria-hidden="true"></span> DB Password: </label>
+										        		   					<?php $attribute = array('name' =>'dbpassword', 'class' => 'form-control', 'id' => 'dbpassword'); echo form_input($attribute); ?>
+										        		   				</div>
+								        		   					</div>
+								        		   				</div>
+								        		   				<?php $attribute = array('class' => 'btn btn-primary pull-right', 'value' => 'Save Settings'); echo form_submit($attribute); ?>
+								        		   			<?php echo form_close(); ?>
+						        		   				</div>
+						        		   			</div>
+						        		   		</div>
+						        		   </div>
+					        			<?php } ?>
 					        		</div>
 				        		</div>
 				        	</div>
